61c9db5c480ae7e67c3f3e703d19109c5528a260
Ganesh-krosuri <vtu29265@veltech.edu.in>
2026-09-14 18:12:48 +0530

    Time: 0 ms (100%), Space: 14.4 MB (6.9%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st;
+
+        for (int i = 0; i < prices.size(); i++) {
+            while (!st.empty() && prices[st.top()] >= prices[i]) {
+                int idx = st.top();
+                st.pop();
+
+                prices[idx] -= prices[i];
+            }
+
+            st.push(i);
+        }
+
+        return prices;
+    }
+};
